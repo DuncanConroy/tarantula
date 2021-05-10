5eea36511649167dd1c52a2dfe0f22a059361349
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-05-10 21:17:02 +0200

    Adds new crate "page_loader" for extracting page loading to an event-driven, asynchronous task

---
+++ page_loader/src/lib.rs
@@ -0,0 +1,17 @@
+// Event-driven page loader
+
+struct PageLoader {
+
+}
+
+impl PageLoader {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
