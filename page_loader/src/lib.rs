c0163526d1e79ddb851301b7994a4000f3136d80
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-05-11 21:03:50 +0200

    implements PageLoaderService to receive commands with URLs

--- page_loader/src/lib.rs
+++ page_loader/src/lib.rs
@@ -1,17 +1,63 @@
 // Event-driven page loader
 
-struct PageLoader {
+use std::sync::{Arc, Mutex};
 
+use tokio::sync::{mpsc, oneshot};
+use tokio::sync::mpsc::{Receiver, Sender};
+
+pub struct PageLoaderService {
+    mpsc_sender: Option<Sender<Command>>,
 }
 
-impl PageLoader {
+impl PageLoaderService {
+    fn new() -> PageLoaderService {
+        PageLoaderService {
+            mpsc_sender: None,
+        }
+    }
 
+    fn init(&mut self) -> Sender<Command> {
+        let buffer_size = num_cpus::get() / 2;
+        let (tx, mut rx) = mpsc::channel(buffer_size);
+        self.mpsc_sender = Some(tx);
+
+        let manager = tokio::spawn(async move {
+            while let Some(event) = rx.recv().await {
+                match event {
+                    Command::LoadPage { url, response_channel } => {
+                        println!("got url: {:?}", url);
+                        response_channel.send(url);
+                    }
+                }
+            }
+        });
+
+        self.mpsc_sender.as_ref().unwrap().clone()
+    }
+}
+
+pub enum Command {
+    LoadPage {
+        url: String,
+        response_channel: oneshot::Sender<String>,
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use tokio::sync::mpsc;
+
+    use crate::*;
+    use crate::Command::LoadPage;
+
+    #[tokio::test]
+    async fn starts_working_on_receiving_command() {
+        let mut page_loader_service = PageLoaderService::new();
+        let tx = page_loader_service.init();
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let send_result = tx.send(LoadPage { url: String::from("https://example.com"), response_channel: resp_tx }).await;
+        assert_eq!(true, send_result.is_ok());
+
+        assert_eq!("https://example.com", resp_rx.await.unwrap());
     }
 }
