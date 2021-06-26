2e91c6accde1ead4b13418a7f36d60ddfb4d0073
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-06-26 16:46:40 +0200

    prepares failing test for fetch_header_command to recursively call

--- page_loader/src/commands/fetch_header_command.rs
+++ page_loader/src/commands/fetch_header_command.rs
@@ -1,20 +1,21 @@
 use std::sync::{Arc, Mutex};
 
 use async_trait::async_trait;
-use hyper::{StatusCode, Uri};
+use hyper::{Body, Response, StatusCode, Uri};
+use mockall::automock;
 
 use crate::page_request::PageRequest;
 
 #[async_trait]
 pub trait FetchHeaderCommand: Sync + Send {
-    async fn fetch_header(&self, page_request: Arc<Mutex<PageRequest>>) -> Result<FetchHeaderResponse, String>;
+    async fn fetch_header(&self, page_request: Arc<Mutex<PageRequest>>, http_client: Box<dyn HttpClient>) -> Result<FetchHeaderResponse, String>;
 }
 
 struct DefaultFetchHeaderCommand {}
 
 #[async_trait]
 impl FetchHeaderCommand for DefaultFetchHeaderCommand {
-    async fn fetch_header(&self, page_request: Arc<Mutex<PageRequest>>) -> Result<FetchHeaderResponse, String> {
+    async fn fetch_header(&self, page_request: Arc<Mutex<PageRequest>>, http_client: Box<dyn HttpClient>) -> Result<FetchHeaderResponse, String> {
         let result = FetchHeaderResponse { redirects: vec![] };
         Ok(result)
     }
@@ -38,12 +39,20 @@ impl FetchHeaderResponse {
     }
 }
 
+#[automock]
+#[async_trait]
+pub trait HttpClient: Sync + Send {
+    async fn head(&self, uri: String) -> Response<Body>;
+}
+
 #[cfg(test)]
 mod tests {
     use std::fmt::{Debug, Formatter, Result};
+    use std::str::FromStr;
     use std::time::Duration;
 
     use mockall::*;
+    use mockall::predicate::eq;
     use tokio::test;
     use tokio::time::Instant;
     use uuid::Uuid;
@@ -86,10 +95,43 @@ mod tests {
         let page_request = PageRequest::new("https://example.com".into(), None, Arc::new(Mutex::new(mock_task_context)));
 
         // when: fetch is invoked
-        let result = command.fetch_header(Arc::new(Mutex::new(page_request))).await;
+        let result = command.fetch_header(Arc::new(Mutex::new(page_request)), Box::new(MockHttpClient::new())).await;
 
         // then: simple response is returned, with no redirects
         assert_eq!(result.is_ok(), true, "Expecting a simple Response");
         assert_eq!(result.unwrap().redirects.len(), 0, "Should not have any redirects");
+    }
+
+    #[tokio::test]
+    async fn should_return_redirect_list_up_to_max_redirects() {
+        // given: simple fetch command is
+        let command = DefaultFetchHeaderCommand {};
+        let mut mock_task_context = MockMyTaskContext::new();
+        let mut task_config = TaskConfig::new("https://example.com".into());
+        task_config.maximum_redirects = 2;
+        mock_task_context.expect_get_config().return_const(Arc::new(Mutex::new(task_config)));
+        let mut mock_http_client = MockHttpClient::new();
+        mock_http_client.expect_head()
+            .with(eq(String::from("https://example.com")))
+            .returning(|_| Response::builder()
+                .status(308)
+                .header("location", "https://first-redirect.example.com")
+                .body(Body::from(""))
+                .unwrap());
+        mock_http_client.expect_head()
+            .with(eq(String::from("https://first-redirect.example.com")))
+            .returning(|_| hyper::Response::builder()
+                .status(308)
+                .header("location", "https://second-redirect.example.com")
+                .body(Body::from(""))
+                .unwrap());
+        let page_request = PageRequest::new("https://example.com".into(), None, Arc::new(Mutex::new(mock_task_context)));
+
+        // when: fetch is invoked
+        let result = command.fetch_header(Arc::new(Mutex::new(page_request)), Box::new(mock_http_client)).await;
+
+        // then: simple response is returned, with maximum_redirects redirects
+        assert_eq!(result.is_ok(), true, "Expecting a simple Response");
+        assert_eq!(result.unwrap().redirects.len(), 2, "Should not have any redirects");
     }
 }
