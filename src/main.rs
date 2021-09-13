ebd2c4d7c05118845e11cd7bff5dccde9e27280a
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-09-13 23:18:30 +0200

    first step in fixing memory issues. still growing after each task :/

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,17 @@
 #[macro_use]
 extern crate rocket;
 
-// use clap::App;
-// use clap::load_yaml;
-// use log::info;
-// use tokio::sync::mpsc;
-
-use clap::App;
-use clap::load_yaml;
-use log::info;
-use tokio::sync::mpsc;
-
-use page_loader::page_loader_service::Command::CrawlDomainCommand;
-use page_loader::page_loader_service::PageLoaderService;
-use server::http::{crawl, RunConfig};
 use std::fs::File;
-use std::process;
 use std::io::Write;
-// use page_loader::page_loader_service::Command::CrawlDomainCommand;
-// use page_loader::page_loader_service::PageLoaderService;
+use std::process;
+
+use log::info;
+
+use page_loader::page_loader_service::PageLoaderService;
+use server::http::crawl;
 
 // A simple type alias so as to DRY.
 pub type DynResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-//
-// #[launch]
-// pub async fn rocket() -> _ {
-//     rocket::build().mount("/", routes![crawl])
-// }
 
 #[rocket::main]
 async fn main() -> DynResult<()> {
@@ -35,55 +20,13 @@ async fn main() -> DynResult<()> {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
     info!("Starting tarantula");
 
-    let _ = rocket::build().mount("/", routes![crawl]).launch().await;
+    let page_loader_tx_channel = PageLoaderService::init();
+
+    let _ = rocket::build()
+        .mount("/", routes![crawl])
+        .manage(page_loader_tx_channel.clone())
+        .launch()
+        .await;
 
     Ok(())
 }
-
-// fn parse_runconfig_from_args() -> Result<RunConfig, &'static str> {
-//     let yaml = load_yaml!("cli.yaml");
-//     let matches = App::from_yaml(yaml).get_matches();
-//
-//     let url = matches.value_of("URL").unwrap();
-//     let mut run_config = RunConfig::new(url.to_string(), None);
-//
-//     run_config.ignore_redirects = matches.is_present("ignore_redirects");
-//     run_config.ignore_robots_txt = matches.is_present("ignore_robots_txt");
-//
-//     if let Some(maximum_depth) = matches.value_of("maximum_depth") {
-//         run_config.maximum_depth = u8::from_str(&maximum_depth).unwrap()
-//     }
-//     if let Some(maximum_redirects) = matches.value_of("maximum_redirects") {
-//         run_config.maximum_redirects = u8::from_str(&maximum_redirects).unwrap()
-//     }
-//     if let Some(_) = matches.value_of("keep_html_in_memory") {
-//         run_config.keep_html_in_memory = true
-//     }
-//
-//     info!("RunConfig: {:#?}", run_config);
-//
-//     Ok(run_config)
-// }
-//
-// async fn process() {
-//     let num_cpus = num_cpus::get();
-//     let tx = PageLoaderService::init();
-//     let (resp_tx, mut resp_rx) = mpsc::channel(num_cpus * 2);
-//
-//     // only used for local command line - todo: refactor out of here
-//     let run_config = parse_runconfig_from_args().unwrap();
-//     let send_result = tx.send(CrawlDomainCommand { url: run_config.url, last_crawled_timestamp: 0, response_channel: resp_tx.clone() }).await;
-//
-//     let manager = tokio::spawn(async move {
-//         let mut responses = 0;
-//         while let Some(page_response) = resp_rx.recv().await {
-//             info!("Received from threads: {:?}", page_response);
-//             responses = responses + 1;
-//             info!(". -> {}", responses);
-//         }
-//     });
-//
-//     manager.await.unwrap();
-//
-//     info!("Finished.");
-// }
