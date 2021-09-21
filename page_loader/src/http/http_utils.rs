b0f079a996eaf780f6b7a88abac54431a9e051fc
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-09-21 22:38:58 +0200

    [BOYSCOUT] - rewrites alternative status codes hashmap to be 1.55 nightly compatible.

--- page_loader/src/http/http_utils.rs
+++ page_loader/src/http/http_utils.rs
@@ -11,17 +11,22 @@ pub fn response_headers_to_map(response: &Response<Body>) -> HashMap<String, Str
         }).collect()
 }
 
+fn build_status_codes() -> HashMap<u16, &'static str> {
+    let mut status_codes = HashMap::new();
+    status_codes.insert(520u16, "[CLOUDFLARE] Web Server Returned an Unknown Error");
+    status_codes.insert(521u16, "[CLOUDFLARE] Web Server Is Down");
+    status_codes.insert(522u16, "[CLOUDFLARE] Connection Timed Out");
+    status_codes.insert(523u16, "[CLOUDFLARE] Origin Is Unreachable");
+    status_codes.insert(524u16, "[CLOUDFLARE] A Timeout Occurred");
+    status_codes.insert(525u16, "[CLOUDFLARE] SSL Handshake Failed");
+    status_codes.insert(526u16, "[CLOUDFLARE] Invalid SSL Certificate");
+    status_codes.insert(527u16, "[CLOUDFLARE] Railgun Error");
+
+    status_codes
+}
+
 pub fn map_status_code(status: hyper::StatusCode) -> StatusCode {
-    let unofficial_codes: HashMap<u16, &str> = HashMap::from([
-        (520u16, "[CLOUDFLARE] Web Server Returned an Unknown Error"),
-        (521u16, "[CLOUDFLARE] Web Server Is Down"),
-        (522u16, "[CLOUDFLARE] Connection Timed Out"),
-        (523u16, "[CLOUDFLARE] Origin Is Unreachable"),
-        (524u16, "[CLOUDFLARE] A Timeout Occurred"),
-        (525u16, "[CLOUDFLARE] SSL Handshake Failed"),
-        (526u16, "[CLOUDFLARE] Invalid SSL Certificate"),
-        (527u16, "[CLOUDFLARE] Railgun Error"),
-    ]);
+    let unofficial_codes: HashMap<u16, &str> = build_status_codes();
 
     let code = status.as_u16();
     let label = if let Some(reason) = status.canonical_reason() {
