35eb4f61fb2c8b8ee14ac41a6fc958eda28c0009
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-10-14 22:22:46 +0200

    [12] - extracts dom_parser integration test into separate file

--- dom_parser/src/lib.rs
+++ dom_parser/src/lib.rs
@@ -60,24 +60,3 @@ impl DomParserService{
             .collect()
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use std::{fs::read_to_string, path::PathBuf};
-
-    use super::*;
-
-    #[test]
-    fn extract_links_returns_correct_links_and_nodes() {
-        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        d = d.parent().unwrap().to_path_buf();
-        d.push("resources/test/example.com.html");
-        let html_file = read_to_string(&d).unwrap();
-
-        let host = "www.example.com";
-        let instance = DomParserService::new(Arc::new(LinkTypeChecker::new(host)));
-        let input = Html::parse_document(html_file.as_str());
-        let result = instance.extract_links("https", host, input.tree);
-        assert_eq!(result.len(), 451 + 79); // href: 451, (data-)?src: 79
-    }
-}
