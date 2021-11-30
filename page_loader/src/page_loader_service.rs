b0571132bbeb54890e1e8c9c66cc62d0adf3d6c1
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-12-01 00:32:22 +0100

    More/better debugging output for tasks

--- page_loader/src/page_loader_service.rs
+++ page_loader/src/page_loader_service.rs
@@ -163,7 +163,8 @@ async fn consume_crawl_result(response_channel: &Sender<CrawlerEvent>, page_craw
     }
     let send_result = response_channel.send(PageEvent { page_response: crawl_result }).await;
     if send_result.is_err() {
-        warn!("Couldn't send PageResponse for PageCrawlCommand id {}", page_crawl_command.get_uuid_clone());
+        warn!("Couldn't send PageResponse for TaskContext {}, PageCrawlCommand id {}, requested_url: {}", page_crawl_command.get_task_context().as_ref().lock().unwrap().get_uuid_clone(),
+            page_crawl_command.get_uuid_clone(), page_crawl_command.get_url_clone());
     } else {
         debug!("all_known_links: {}", page_crawl_command.get_task_context().lock().unwrap().get_all_crawled_links().lock().unwrap().len());
         debug!("all_tasked_links: {}", page_crawl_command.get_task_context().lock().unwrap().get_all_tasked_links().lock().unwrap().len());
